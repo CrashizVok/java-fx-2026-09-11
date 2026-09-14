41e3e6ae4ec63096a059aa5fb73718d6ee5b48d8
Huggus Buggus <regoronyecz1@gmail.com>
2026-09-14 10:34:01 +0200

    hozzaad nem jó

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +14,7 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 300);
         stage.setTitle("Hello!");
-
+        stage.getIcons().add(new Image("file:src/main/assets/negyzet.png"));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,52 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+import java.util.Arrays;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    String[] nowAvailable = null;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private RadioButton radioPiros, radioZold, radioKek, radioNegyzet, radioKor, radioHaromszog;
+
+    @FXML
+    protected void hozzaadFunction() {
+        getColorRadio();
+        getShaved();
+
+        System.out.println(Arrays.toString(nowAvailable));
+
+    }
+
+    protected void getColorRadio(){
+        if (radioKek.isSelected()){
+            nowAvailable[0] = "kek";
+        }
+        else if(radioZold.isSelected()){
+            nowAvailable[0] = "zold";
+        }
+        else if(radioPiros.isSelected()){
+            nowAvailable[0] = "piros";
+        }
+        else {
+            System.out.println("Nagy baj van");
+        }
+    }
+
+    protected void getShaved(){
+        if (radioNegyzet.isSelected()){
+            nowAvailable[1] = "negyzet";
+        }
+        else if (radioKor.isSelected()) {
+            nowAvailable[1] = "kor";
+        }
+        else if (radioHaromszog.isSelected()) {
+            nowAvailable[1] = "haromszog";
+        }
+        else {
+            System.out.println("Nagy baj van, megint");
+        }
     }
 }
